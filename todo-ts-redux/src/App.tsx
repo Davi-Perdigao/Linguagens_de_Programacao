0c21e2558773224072731b329632d8bdab8c1ca2
Davi Perdigão <davivcperdigao@gmail.com>
2022-08-31 09:31:31 -0300

    ajustes To-Do List

--- todo-ts-redux/src/App.tsx
+++ todo-ts-redux/src/App.tsx
@@ -33,54 +33,7 @@ function App() {
             </Heading>
 
             <TaskList />
-            
-            <Flex position='absolute' bottom='5'>
-                <Link href='https://github.com/linhous' target='_blank' >
-                    <IconButton 
-                    aria-label='Github'
-                    icon={<FaGithub/>}
-                    isRound={true}
-                    size='md'
-                    m='1'
-                /> 
-                </Link>
-                <Link href='https://www.linkedin.com/in/paulo-vitor-nascimento-656b0145/' target='_blank'>
-                    <IconButton 
-                    aria-label='Linkedin'
-                    icon={<FaLinkedin/>}
-                    isRound={true}
-                    size='md'
-                    m='1'
-                /> 
-                </Link>
-                <Link href='https://www.instagram.com/linhous/' target='_blank'>
-                    <IconButton 
-                    aria-label='Instagram'
-                    icon={<FaInstagram/>}
-                    isRound={true}
-                    size='md'
-                    m='1'
-                /> 
-                </Link>
-                <Link href='https://twitter.com/paulinho_nas' target='_blank'>
-                    <IconButton 
-                    aria-label='Twitter'
-                    icon={<FaTwitter/>}
-                    isRound={true}
-                    size='md'
-                    m='1'
-                /> 
-                </Link>
-                <Link href='https://www.facebook.com/nascimento.paulo.vitor' target='_blank'>
-                    <IconButton
-                    aria-label='Facebook' 
-                    icon={<FaFacebook/>}
-                    isRound={true}
-                    size='md'
-                    m='1'
-                /> 
-                </Link>
-            </Flex>
+
         </VStack>
     );
 }
